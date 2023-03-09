7014b794feb23b8fd3d45baba765b5314ae21e70
GyanGartia <gyangartia55@gmail.com>
2023-03-09 13:16:37 +0530

    initial commit

---
+++ Controllers/AccountControler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class AccountControler : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();  
+        }
+    }
+}
