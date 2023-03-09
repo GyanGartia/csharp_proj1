b84857880c16f6a5cf38dbad9535567ac5288e9f
GyanGartia <gyangartia55@gmail.com>
2023-03-09 16:16:33 +0530

    added some files

---
+++ Components/Comments.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Components
+{
+    public class Comments : ViewComponent
+    {
+        public IViewComponentResult Invoke(int id)
+        { 
+            List<string>  comments = new List<string>() { "Good", "Great", "Keep it up"};
+            return View(comments);  
+        }
+    }
+}
